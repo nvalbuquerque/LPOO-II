b38240aa9964cd7f4f32f6c0af704d038035e289
Nathalia Lyra Varela de Albuquerque <nathalialyra@ufpr.br>
2026-03-03 20:04:14 -0300

    Refactor area calculation methods to return values

--- Tarefa1/CalculaArea.java
+++ Tarefa1/CalculaArea.java
@@ -2,6 +2,7 @@ import java.lang.Math;
 import java.util.Scanner;
 
 public class CalculaArea {
+    static int op;
     static double lado1;
     static int lado2;
     static double raio;
@@ -17,29 +18,75 @@ public class CalculaArea {
         }
     }
 
-    static public void calcularAreaRetangulo (double lado1, double lado2) {
-        if (lado1 < 0 || lado2 < 0) {
-            throws NumeroNegativoException("Não são permitidos números negativos.");
-        } else {
-            areaRetangulo = lado1 * lado2;
+    static public double calcularAreaRetangulo (double lado1, double lado2) throws NumeroNegativoException {
+            
+            if (lado1 < 0 || lado2 < 0) {
+                throw new NumeroNegativoException("Não são permitidos números negativos.");
+            }
+            return areaRetangulo = lado1 * lado2;
+    }
+    
+    static public double calcularAreaCircunferencia (double raio) throws NumeroNegativoException {
+        if (raio < 0) {
+            throw new NumeroNegativoException("Não são permitidos números negativos.");
         }
-        
+        return areaCircunferencia = Math.pow(raio,2) * Math.PI;
     }
     
-    static public void calcularAreaCircunferencia (double raio) {
-        areaCircunferencia = Math.pow(raio,2) * Math.PI;
-    }
-    
-    static public void calcularAreaTriangulo (double base, double altura) {
-        areaTriangulo = (base * altura)/2;
+    static public double calcularAreaTriangulo (double base, double altura) throws NumeroNegativoException {
+        if (base < 0 || altura < 0) {
+                throw new NumeroNegativoException("Não são permitidos números negativos.");
+            }
+        return areaTriangulo = (base * altura) / 2;
     }
     
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Insira valor do lado 1:");
-        double lado1 = scanner.nextDouble();
-
-        System.out.println("Insira valor do lado 2:");
-        double lado2 = scanner.nextDouble();
+        
+        System.out.print("=== MENU ===\n");
+        System.out.print("1. Área do retângulo\n");
+        System.out.print("2. Área da circunferência\n");
+        System.out.print("3. Área do triângulo\n");
+        System.out.println("Escolha a opção a ser calculada:");
+        op = scanner.nextInt();
+        
+        switch(op) {
+            case 1:
+                System.out.println("Insira valor do lado 1:");
+                double lado1 = scanner.nextDouble();
+        
+                System.out.println("Insira valor do lado 2:");
+                double lado2 = scanner.nextDouble();
+     
+                try {
+                    System.out.println(calcularAreaRetangulo(lado1, lado2));
+                } catch (NumeroNegativoException e) {
+                    System.out.println("Erro: " + e.getMessage());
+                }
+                break;
+            case 2:
+                System.out.println("Insira valor do raio:");
+                double raio = scanner.nextDouble();
+    
+                try {
+                    System.out.println(calcularAreaCircunferencia(raio));
+                } catch (NumeroNegativoException e) {
+                    System.out.println("Erro: " + e.getMessage());
+                }
+                break;
+            case 3: 
+                System.out.println("Insira valor da base:");
+                double base = scanner.nextDouble();
+        
+                System.out.println("Insira valor da altura:");
+                double altura = scanner.nextDouble();
+     
+                try {
+                    System.out.println(calcularAreaTriangulo(base, altura));
+                } catch (NumeroNegativoException e) {
+                    System.out.println("Erro: " + e.getMessage());
+                }
+                break;
+        }
     }
 }
